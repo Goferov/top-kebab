d0e483e6ddd5a6c4e27b442508f550967106267f
Goferov <marcingodfryd@gmail.com>
2024-12-12 22:39:45 +0100

    Create routes for api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('restaurants', RestaurantController::class);
+Route::apiResource('restaurants.reviews', ReviewController::class)
+    ->scoped(['review' => 'restaurant']);
