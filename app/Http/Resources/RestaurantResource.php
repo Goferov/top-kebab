42e4493647aeb5ab276da9e66259b46dcf77766e
Goferov <marcingodfryd@gmail.com>
2024-12-14 01:12:53 +0100

    Create resources for all models

---
+++ app/Http/Resources/RestaurantResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RestaurantResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'image' => $this->image,
+            'email' => $this->email,
+            'website' => $this->website,
+            'phone' => $this->phone,
+            'address' => new AddressResource($this->whenLoaded('address')),
+        ];
+    }
+}
