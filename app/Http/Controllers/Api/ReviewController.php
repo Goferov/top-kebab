3294628d9b33e3b5e2e8d2f56e413be832e9ff16
Goferov <marcingodfryd@gmail.com>
2024-12-17 22:38:01 +0100

    Implement Review list, store, show and delete in ReviewController

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\RestaurantResource;
+use App\Http\Resources\ReviewResource;
+use App\Models\Restaurant;
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -10,25 +14,41 @@ class ReviewController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Restaurant $restaurant)
     {
-        //
+        $reviews = $restaurant->reviews()->latest();
+
+        return ReviewResource::collection(
+            $reviews->paginate()
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Restaurant $restaurant)
     {
-        //
+
+        $validatedData = $request->validate([
+            'rate' => 'required|integer|min:1|max:5',
+            'review' => 'required|string',
+        ]);
+
+        $review = $restaurant->reviews()->create([
+            'user_id' => 1,
+            'rate'    => $validatedData['rate'],
+            'review'  => $validatedData['review'],
+        ]);
+
+        return new ReviewResource($review);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Restaurant $restaurant, Review $review)
     {
-        //
+        return new ReviewResource($review);
     }
 
     /**
@@ -42,8 +62,9 @@ class ReviewController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Restaurant $restaurant, Review $review)
     {
-        //
+        $review->delete();
+        return response(status: 204);
     }
 }
