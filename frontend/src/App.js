6cda546d486fac3f3743a7e3a3f0e074bc1fac0e
Goferov <marcingodfryd@gmail.com>
2025-01-14 22:01:30 +0100

    Add routes for restaurant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import RestaurantList from './components/RestaurantList';
 import Login from './components/Login';
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import { ModalProvider } from './services/modal';
-// import RestaurantDetails from './components/RestaurantDetails';
+import RestaurantList from './components/RestaurantList';
+import RestaurantDetails from './components/RestaurantDetails'
 
 function App() {
   return (
@@ -17,6 +17,8 @@ function App() {
                   <Route element={<MainLayout />}>
                       <Route path="/" element={<Home />} />
                       <Route path="/login" element={<Login />} />
+                      <Route path="/restaurant" element={<RestaurantList />} />
+                      <Route path="/restaurant/:id" element={<RestaurantDetails />} />
                       <Route path="/contact" element={<Contact />} />
                   </Route>
               </Routes>
