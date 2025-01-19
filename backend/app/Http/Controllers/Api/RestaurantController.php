7ac928b980beae46632bf6971f2856f20a6411d1
Goferov <marcingodfryd@gmail.com>
2025-01-19 19:04:13 +0100

    Add image handler im RestaurantController

--- backend/app/Http/Controllers/Api/RestaurantController.php
+++ backend/app/Http/Controllers/Api/RestaurantController.php
@@ -8,6 +8,7 @@ use App\Models\Address;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
 
 class RestaurantController extends Controller
 {
@@ -91,16 +92,12 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
-//        DB::transaction(function() {
-//            //
-//        });
-
         Gate::authorize('create', Restaurant::class);
 
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'nullable|string',
-            'image' => 'nullable|string|max:255',
+            'image' => 'nullable|string',
             'email' => 'nullable|email|max:255',
             'website' => 'nullable|url|max:255',
             'phone' => 'nullable|string|max:16',
@@ -119,18 +116,47 @@ class RestaurantController extends Controller
             'apartment_no' => $validatedData['address']['apartment_no'] ?? null,
         ]);
 
-        $restaurant = Restaurant::create([
+        $restaurant = new Restaurant([
             'name' => $validatedData['name'],
             'description' => $validatedData['description'] ?? null,
-            'image' => $validatedData['image'] ?? null,
+            'image' => $this->handleImage(null, $validatedData['image'] ?? null), // Obsługa obrazu
             'email' => $validatedData['email'] ?? null,
             'website' => $validatedData['website'] ?? null,
             'phone' => $validatedData['phone'] ?? null,
             'address_id' => $address->id,
+            'publicate' => true,
         ]);
+
+        $restaurant->save();
 
         return new RestaurantResource($restaurant->load('address'));
     }
+
+
+    private function handleImage($restaurant, $imageData = null, $deleteFile = false)
+    {
+        if ($deleteFile && $restaurant->image) {
+            if (Storage::disk('public')->exists("uploads/{$restaurant->image}")) {
+                Storage::disk('public')->delete("uploads/{$restaurant->image}");
+            }
+            return null;
+        }
+
+        if ($imageData) {
+            $fileName = time() . '_image.png';
+
+            Storage::disk('public')->put("uploads/{$fileName}", base64_decode($imageData));
+
+            if ($restaurant && $restaurant->image && Storage::disk('public')->exists("uploads/{$restaurant->image}")) {
+                Storage::disk('public')->delete("uploads/{$restaurant->image}");
+            }
+
+            return $fileName;
+        }
+
+        return $restaurant->image ?? null;
+    }
+
 
     /**
      * Display the specified resource.
@@ -141,16 +167,16 @@ class RestaurantController extends Controller
         return new RestaurantResource($restaurant->load(['address', 'reviews.user']));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
+
     public function update(Request $request, Restaurant $restaurant)
     {
         Gate::authorize('update', $restaurant);
+
         $validatedData = $request->validate([
             'name' => 'sometimes|string|max:255',
             'description' => 'nullable|string',
-            'image' => 'nullable|string|max:255',
+            'image' => 'nullable|string', // Obsługa obrazu w Base64
             'email' => 'nullable|email|max:255',
             'website' => 'nullable|url|max:255',
             'phone' => 'nullable|string|max:16',
@@ -159,13 +185,13 @@ class RestaurantController extends Controller
             'address.postal_code' => 'sometimes|string|max:20',
             'address.house_no' => 'sometimes|string|max:10',
             'address.apartment_no' => 'nullable|string|max:10',
+            'delete_file' => 'nullable|boolean',
         ]);
-
 
         $restaurant->update([
             'name' => $validatedData['name'],
             'description' => $validatedData['description'] ?? null,
-            'image' => $validatedData['image'] ?? null,
+            'image' => $this->handleImage($restaurant, $validatedData['image'] ?? null, $validatedData['delete_file'] ?? false),
             'email' => $validatedData['email'] ?? null,
             'website' => $validatedData['website'] ?? null,
             'phone' => $validatedData['phone'] ?? null,
@@ -173,16 +199,19 @@ class RestaurantController extends Controller
 
         if ($restaurant->address) {
             $restaurant->address->update([
-                'street' => $validatedData['address']['street'],
-                'city' => $validatedData['address']['city'],
-                'postal_code' => $validatedData['address']['postal_code'],
-                'house_no' => $validatedData['address']['house_no'],
-                'apartment_no' => $validatedData['address']['apartment_no'] ?? null,
+                'street' => $validatedData['address']['street'] ?? $restaurant->address->street,
+                'city' => $validatedData['address']['city'] ?? $restaurant->address->city,
+                'postal_code' => $validatedData['address']['postal_code'] ?? $restaurant->address->postal_code,
+                'house_no' => $validatedData['address']['house_no'] ?? $restaurant->address->house_no,
+                'apartment_no' => $validatedData['address']['apartment_no'] ?? $restaurant->address->apartment_no,
             ]);
         }
 
         return new RestaurantResource($restaurant->load('address'));
     }
+
+
+
 
     /**
      * Remove the specified resource from storage.
