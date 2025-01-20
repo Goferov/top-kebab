6a377fb5c84993c83ce04fe4b3dc19b4b98ffeb0
Goferov <marcingodfryd@gmail.com>
2025-01-20 22:59:40 +0100

    Remove login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import ProtectedRoute from './components/ProtectedRoute';
-import Login from './components/Login';
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Panel from "./components/Panel";
@@ -24,7 +23,6 @@ function App() {
                     <Routes>
                         <Route element={<MainLayout />}>
                             <Route path="/" element={<Home />} />
-                            <Route path="/login" element={<Login />} />
                             <Route path="/restaurant" element={<RestaurantList />} />
                             <Route path="/restaurant/:id" element={<RestaurantDetails />} />
                             <Route path="/contact" element={<Contact />} />
