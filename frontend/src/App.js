84208fbe181a7ad4d3985d447c681f0a0324d989
Goferov <marcingodfryd@gmail.com>
2025-01-21 00:52:21 +0100

    Add error 404

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import RestaurantList from './components/RestaurantList';
 import RestaurantDetails from './components/RestaurantDetails';
 import AdminRestaurantList from "./components/AdminRestaurantList";
 import AddRestaurant from "./components/AddRestaurant";
+import Error404 from "./components/Error404";
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
                             <Route path="/restaurantList" element={<ProtectedRoute adminOnly><AdminRestaurantList /></ProtectedRoute>} />
                             <Route path="/addRestaurant" element={<ProtectedRoute adminOnly><AddRestaurant /></ProtectedRoute>} />
                             <Route path="/addRestaurant/:id" element={<ProtectedRoute adminOnly><AddRestaurant /></ProtectedRoute>} />
+                            <Route path="*" element={<Error404 />} />
                         </Route>
                     </Routes>
                 </Router>
